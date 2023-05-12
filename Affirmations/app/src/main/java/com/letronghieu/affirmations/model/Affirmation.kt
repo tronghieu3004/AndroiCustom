1da1d30f50b4d58471a3d7ea9fb85aaf515c4d37
Le Trong Hieu UTM <letronghieu3004.utm@gmail.com>
2023-05-12 22:19:50 +0700

    bai1(12052023)

---
+++ Affirmations/app/src/main/java/com/letronghieu/affirmations/model/Affirmation.kt
@@ -0,0 +1,6 @@
+package com.letronghieu.affirmations.model
+
+import android.security.identity.AccessControlProfileId
+import java.util.ListResourceBundle
+
+data class Affirmation (val stringResourceId: Int)
